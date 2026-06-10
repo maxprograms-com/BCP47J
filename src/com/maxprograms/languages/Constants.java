6d057d3140a6131c1a6c7cbbc19b9e18856079a5
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-06-10 07:24:49 -0300

    Updated XMLJava

--- src/com/maxprograms/languages/Constants.java
+++ src/com/maxprograms/languages/Constants.java
@@ -16,5 +16,5 @@ public class Constants {
     }
     
     public static final String VERSION = "4.0.0";
-    public static final String BUILD = "20260609_1210";
+    public static final String BUILD = "20260610_0724";
 }
