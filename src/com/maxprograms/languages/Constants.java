6391313b85613f44f70640b5074642f8ca106083
Rodolfo M. Raya <rmraya@maxprograms.com>
2026-09-26 08:33:43 -0300

    Updated language list

--- src/com/maxprograms/languages/Constants.java
+++ src/com/maxprograms/languages/Constants.java
@@ -15,6 +15,6 @@ public class Constants {
         // do not instantiate
     }
     
-    public static final String VERSION = "4.3.0";
-    public static final String BUILD = "20260920_1830";
+    public static final String VERSION = "4.3.1";
+    public static final String BUILD = "20260926_0833";
 }
